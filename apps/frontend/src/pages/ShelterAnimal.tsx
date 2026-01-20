59d53223cca06d1a5cb1baba94faed94b5189e76
eddy <eddy.prouveur@oclock.school>
2026-01-20 19:04:08 +0100

    page animaux par refuge

--- apps/frontend/src/pages/ShelterAnimal.tsx
+++ apps/frontend/src/pages/ShelterAnimal.tsx
@@ -1,36 +1,56 @@
 import { Link, useParams } from "react-router-dom";
-import { mockShelters } from "../mocks/Shelter.mock";
-import { mockAnimalList } from "../mocks/animalList.mock";
+import { useEffect, useState } from "react";
 import _AnimalCard from "../components/AnimalCard";
 import BackBanner from "../components/ui/BackBanner";
 
-// TODO connecter à la BDD
+const API_URL = import.meta.env.VITE_API_URL;
 
 const ShelterAnimalsPage = () => {
-  const { id } = useParams(); // récupère l'id de l'URL
-  const shelter = mockShelters.find((s) => s.pfc_user_id.toString() === id);
+  const { id } = useParams<{ id: string }>();
+  console.log("useParams id côté frontend (ShelterAnimalsPage):", id);
+
+  const [shelter, setShelter] = useState<any | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchShelter = async () => {
+      try {
+        if (!id) return; // évite l'appel avec undefined
+        console.log("Appel API ShelterAnimals avec id:", id);
+        const res = await fetch(`${API_URL}/shelters/${id}`);
+        if (!res.ok) throw new Error("Erreur HTTP " + res.status);
+        const data = await res.json();
+        console.log("Réponse backend ShelterAnimals:", data);
+        setShelter(data);
+      } catch (err) {
+        console.error("Erreur API:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchShelter();
+  }, [id]);
+
+  if (loading) return <p>Chargement...</p>;
+  if (!shelter) return <p>Refuge introuvable</p>;
 
   return (
-  <div className="bg-bgapp font-openSans text-gray-800">
-    <BackBanner to="/refuges" />
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-6">
-        {shelter
-          ? `Animaux du ${shelter.shelter_name}`
-          : "Animaux du refuge"}
-      </h1>
+    <div className="bg-bgapp font-openSans text-gray-800">
+      <BackBanner to="/refuges" />
+      <div className="p-8">
+        <h1 className="text-3xl font-bold mb-6">
+          {`Animaux du ${shelter.shelterName}`}
+        </h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
-        {mockAnimalList
-          .filter((animal) => animal.pfc_user_id.toString() === id)
-          .map((animal) => (
-            < Link key={animal.id} to={`/animaux/${animal.id}`}>
-            <_AnimalCard {...animal} />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+          {shelter.user?.animals?.map((animal: any) => (
+            <Link key={animal.id} to={`/animaux/${animal.id}`}>
+              <_AnimalCard {...animal} />
             </Link>
           ))}
+        </div>
       </div>
     </div>
-  </div> 
   );
 };
 
