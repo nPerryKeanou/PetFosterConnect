253b13e99d9af2cbf487ab7d6522367321fab73f
Vincent-Couturier <vincent.couturier@oclock.school>
2026-01-21 14:02:53 +0100

    creation logout + signin + gestion cookies

--- apps/frontend/src/auth/authContext.tsx
+++ apps/frontend/src/auth/authContext.tsx
@@ -1,9 +1,17 @@
-import { createContext, type ReactNode, useContext, useState } from "react";
+import {
+  createContext,
+  type ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { api } from "../api/api";
 
 // Définition du type du contexte
 interface AuthContextType {
   isLoggedIn: boolean;
   setIsLoggedIn: (val: boolean) => void;
+  logout: () => Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -11,9 +19,30 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 // Provider à envelopper autour de ton App
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [_user, _setUserrr] = useState(null);
+
+  const logout = async () => {
+    await api.post("/auth/logout"); // supprime le cookie
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await api.get("/auth/me", {
+          withCredentials: true,
+        });
+        setIsLoggedIn(true);
+      } catch {
+        setIsLoggedIn(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, logout }}>
       {children}
     </AuthContext.Provider>
   );
