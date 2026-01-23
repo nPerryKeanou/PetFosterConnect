ab24d72578aa53f175f60869e3f3a0099f8b1715
eddy <eddy.prouveur@oclock.school>
2026-01-24 00:42:19 +0100

    Ajout du composant BurgerMenu

---
+++ apps/frontend/src/components/ui/BurgerMenu.tsx
@@ -0,0 +1,21 @@
+
+
+interface BurgerMenuProps {
+  onOpen: () => void;
+}
+
+export default function BurgerMenu({ onOpen }: BurgerMenuProps) {
+  return (
+    <button
+      className="md:hidden p-4 absolute top-2 left-2 z-50"
+      onClick={onOpen}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+        viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+        className="w-8 h-8">
+        <path strokeLinecap="round" strokeLinejoin="round"
+          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+      </svg>
+    </button>
+  );
+}
